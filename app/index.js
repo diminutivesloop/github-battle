75f6eed7607bf354c440e33d4d8d538770713913
Nathaniel Palmer <nathaniel.palmer@simplisafe.com>
2021-09-02 21:11:32 -0400

    9 - Battle Instructions

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Popular from "./components/Popular";
 import './index.css';
+import Popular from "./components/Popular";
+import Battle from "./components/Battle";
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="container">
-				<Popular />
+				<Battle />
 			</div>
 		)
 	}	
