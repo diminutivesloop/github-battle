b7731e6a86cb389ba9fd387c321ba75981b8dd37
Nathaniel Palmer <nathaniel.palmer@simplisafe.com>
2021-10-08 15:15:04 -0400

    15 - results data

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,12 +2,34 @@ import React from 'react'
 import { battle } from '../utils/api'
 
 export default class Results extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			winner: null,
+			loser: null,
+			error: null,
+			loading: true
+		}
+	}
+
 	componentDidMount() {
 		const { playerOne, playerTwo } = this.props
 
 		battle([ playerOne, playerTwo ])
 			.then((players) => {
-				console.log(players)
+				this.setState({
+					winner: players[0],
+					loser: players[1],
+					error: null,
+					loading: false
+				})
+			})
+			.catch(({ message }) => {
+				this.setState({
+					error: message,
+					loading: false
+				})
 			})
 	}
 
@@ -15,7 +37,7 @@ export default class Results extends React.Component {
 		return (
 			<div>
 				Results
-				<pre>{JSON.stringify(this.props, null, 2)}</pre>
+				<pre>{JSON.stringify(this.state, null, 2)}</pre>
 			</div>
 		)
 	}
