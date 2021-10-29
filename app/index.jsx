7367331bbae931b991ba5aea2aea57a13d1ffe4d
Nathaniel Palmer <nathaniel.palmer@simplisafe.com>
2021-10-29 15:57:49 -0400

    24 - theme provider

--- app/index.jsx
+++ app/index.jsx
@@ -3,13 +3,28 @@ import ReactDom from "react-dom";
 import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
+import { ThemeProvider } from "./contexts/theme";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () =>
+        this.setState((prevState) => ({
+          theme: prevState.theme === "light" ? "dark" : "light",
+        })),
+    };
+  }
+
   render() {
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Battle />
+        </div>
+      </ThemeProvider>
     );
   }
 }
